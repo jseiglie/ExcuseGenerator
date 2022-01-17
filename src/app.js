fee5b731d2476e18355fb5ffcadb0c1505a1cb47
jseiglie <javierseiglie@gmail.com>
2022-01-17 11:41:08 +0000

    Optimized

--- src/app.js
+++ src/app.js
@@ -1,43 +1,25 @@
 window.onload = function() {
-  let who = ["The dog", "A raccoon", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "abduct", "crushed", "broke"];
-  let what = ["my homework", "my laptop", "my brain", "the keys", "the car"];
-  let when = [
+  const who = ["The dog", "A raccoon", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "abduct", "crushed", "broke"];
+  const what = ["my homework", "my laptop", "my brain", "the keys", "the car"];
+  const when = [
     "before the class",
     "right on time",
     "when I finished",
     "during my lunch",
     "while I was praying"
   ];
-
-  let selWho = Math.floor(Math.random() * who.length);
-  let rSelWho = who[selWho];
-  //console.log(rSelWho);
-
-  let selAct = Math.floor(Math.random() * action.length);
-  let rSelAct = action[selAct];
-  //console.log(rSelAct);
-
-  let selWhat = Math.floor(Math.random() * what.length);
-  let rSelWhat = what[selWhat];
-  //console.log(rSelWhat);
-
-  let selWhen = Math.floor(Math.random() * when.length);
-  let rSelWhen = when[selWhen];
-  //console.log(rSelWhen);
-
-  console.log(`${rSelWho} ${rSelAct} ${rSelWhat} ${rSelWhen}`);
-
-  ////////////////////////////////////
-  let select = array => {
-    let selectarray = Math.floor(Math.random() * array.length);
-    return array[selectarray];
+  const excuse = [who, action, what, when];
+  const RandomIndex = arr => Math.floor(Math.random() * arr.length);
+  const RandomItem = (arr, i) => arr[i];
+  const GenerateString = arr => RandomItem(arr, RandomIndex(arr));
+  const generateExcuse = arr => {
+    let str = "";
+    for (const val of arr) {
+      str += GenerateString(val) + " ";
+    }
+    return str;
   };
-
-  console.log(
-    `${select(who)} ${select(action)} ${select(what)} ${select(when)}`
-  );
+  console.log(generateExcuse(excuse));
+  console.log("under pressure dun dun dun durudurun");
 };
-
-//arrArr = [[who], [what]];
-//console.log(arrArr);
