45aacc6f035b4e5cfdd49ffd6f31dae9b253e32f
jseiglie <javierseiglie@gmail.com>
2022-01-18 09:20:54 +0000

    deploy

--- src/app.js
+++ src/app.js
@@ -1,25 +1,31 @@
-window.onload = function() {
-  const who = ["The dog", "A raccoon", "My grandma", "His turtle", "My bird"];
-  const action = ["ate", "peed", "abduct", "crushed", "broke"];
-  const what = ["my homework", "my laptop", "my brain", "the keys", "the car"];
-  const when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  const excuse = [who, action, what, when];
-  const RandomIndex = arr => Math.floor(Math.random() * arr.length);
-  const RandomItem = (arr, i) => arr[i];
-  const GenerateString = arr => RandomItem(arr, RandomIndex(arr));
-  const generateExcuse = arr => {
-    let str = "";
-    for (const val of arr) {
-      str += GenerateString(val) + " ";
-    }
-    return str;
-  };
-  console.log(generateExcuse(excuse));
-  console.log("under pressure dun dun dun durudurun");
+const who = ["The dog", "A raccoon", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "abduct", "crushed", "broke"];
+const what = ["my homework", "my laptop", "my brain", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+const excuse = [who, action, what, when];
+const RandomIndex = arr => Math.floor(Math.random() * arr.length);
+const RandomItem = (arr, i) => arr[i];
+const GenerateString = arr => RandomItem(arr, RandomIndex(arr));
+const generateExcuse = arr => {
+  let str = "";
+  for (const val of arr) {
+    str += GenerateString(val) + " ";
+  }
+  return str;
 };
+console.log(generateExcuse(excuse));
+console.log("under pressure dun dun dun durudurun");
+
+window.onload = function() {
+  document.querySelector("#myExcuse").innerHTML = generateExcuse(excuse);
+};
+
+document.querySelector("#myButtom").addEventListener("click", function() {
+  document.querySelector("#myExcuse").innerHTML = generateExcuse(excuse);
+});
